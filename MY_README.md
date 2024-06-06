8d34dd897369f29550d16de79be552e07c3e79e9
Vasileva Ludmila <thegoldenbeetle@yandex.ru>
2024-06-06 02:49:22 +0200

    Added dbscan experiment

--- MY_README.md
+++ MY_README.md
@@ -8,4 +8,10 @@ Build docker
 Run docker
 2. docker-compose -f ./docker-compose.yaml run --rm workspace
 
+### Evaluation
+
+Semantic inference authors model:
+
+- `python3 infer.py -d /data/ -l preds_semantic_cenet_authors/ -m Final\ result/512-594/ -s valid`
+
 
--- modules/user.py
+++ modules/user.py
@@ -47,6 +47,9 @@ class User():
         if self.ARCH["train"]["pipeline"] == "res":
             from modules.network.ResNet import ResNet_34
             self.model = ResNet_34(self.parser.get_n_classes(), self.ARCH["train"]["aux_loss"])
+            self.model = self.model
+            # self.get_model_size()
+            
 
             def convert_relu_to_softplus(model, act):
                 for child_name, child in model.named_children():
@@ -91,9 +94,20 @@ class User():
       self.gpu = True
       self.model.cuda()
 
+  def get_model_size(self):
+      size_model = 0
+      for param in self.model.parameters():
+       if param.data.is_floating_point():
+        size_model += param.numel() * torch.finfo(param.data.dtype).bits
+       else:
+        size_model += param.numel() * torch.iinfo(param.data.dtype).bits
+      print(f"model size: {size_model} / bit | {size_model / 8e6:.2f} / MB")
+      exit()
+
   def infer(self):
     cnn = []
     knn = []
+    dbscan = []
     if self.split == None:
 
         self.infer_subset(loader=self.parser.get_train_set(),
@@ -109,7 +123,7 @@ class User():
 
     elif self.split == 'valid':
         self.infer_subset(loader=self.parser.get_valid_set(),
-                        to_orig_fn=self.parser.to_original, cnn=cnn, knn=knn)
+                        to_orig_fn=self.parser.to_original, cnn=cnn, knn=knn, dbscan=dbscan)
     elif self.split == 'train':
         self.infer_subset(loader=self.parser.get_train_set(),
                         to_orig_fn=self.parser.to_original, cnn=cnn, knn=knn)
@@ -118,12 +132,13 @@ class User():
                         to_orig_fn=self.parser.to_original, cnn=cnn, knn=knn)
     print("Mean CNN inference time:{}\t std:{}".format(np.mean(cnn), np.std(cnn)))
     print("Mean KNN inference time:{}\t std:{}".format(np.mean(knn), np.std(knn)))
+    print("Mean KNN inference time:{}\t std:{}".format(np.mean(dbscan), np.std(dbscan)))
     print("Total Frames:{}".format(len(cnn)))
     print("Finished Infering")
 
     return
 
-  def infer_subset(self, loader, to_orig_fn,cnn,knn):
+  def infer_subset(self, loader, to_orig_fn,cnn,knn,dbscan=None):
     # switch to evaluate mode
 
     self.model.eval()
@@ -214,7 +229,111 @@ class User():
         # map to original label
         pred_np = to_orig_fn(pred_np)
 
+        preds, time_dbscan = self.dbscan_panoptic(pred_np, proj_argmax, proj_range, p_x, p_y)
+        dbscan.append(time_dbscan)
+
         # save scan
         path = os.path.join(self.logdir, "sequences",
                             path_seq, "predictions", path_name)
         pred_np.tofile(path)
+
+
+  def dbscan_panoptic(self, pred_np, proj_argmax, proj_range, p_x, p_y):
+
+        # print("points: ", unproj_xyz)
+        # dbscan
+        import hdbscan
+        from sklearn.cluster import DBSCAN
+
+        start_time = time.time()
+        # X = unproj_xyz[0][:npoints].cuda()
+
+        cars_idx = proj_argmax == 1
+        bicycle_idx = proj_argmax == 2
+        motorcycle_idx = proj_argmax == 3
+        truch_idx = proj_argmax == 4
+        other_vehicle_idx = proj_argmax == 5
+        person_idx = proj_argmax == 6
+        bicyclist_idx = proj_argmax == 7
+        motorcyclist_idx = proj_argmax == 8
+        
+        # cars_idx = unproj_argmax == 1
+        # bicycle_idx = unproj_argmax == 2
+        # motorcycle_idx = unproj_argmax == 3
+        # truch_idx = unproj_argmax == 4
+        # other_vehicle_idx = unproj_argmax == 5
+        # person_idx = unproj_argmax == 6
+        # bicyclist_idx = unproj_argmax == 7
+        # motorcyclist_idx = unproj_argmax == 8
+
+        def cluster(proj_range, idx, k):
+           Z = proj_range[idx]
+           Y, X = torch.where(idx)
+           data = torch.stack((X, Y, Z))
+           # data = torch.stack((Z,))
+           if torch.numel(data) < 5:
+              return torch.zeros(proj_range.shape)
+           data = torch.transpose(data, 0, 1)
+
+        #    data = torch.unsqueeze(data, 0)
+        # #    print("data: ", data.shape)
+        #    dist = torch.cdist(data, data)
+        #    if torch.numel(dist) == 1:
+        #       return None
+        #    print("cdist: ", dist.shape)
+        #    cluster = DBSCAN(metric="precomputed").fit(dist[0].detach().cpu().numpy())
+        #    cluster = hdbscan.HDBSCAN(metric="precomputed", allow_single_cluster=True).fit(dist[0].detach().cpu().numpy().astype('double'))
+           cluster = hdbscan.HDBSCAN(allow_single_cluster=True).fit(data.detach().cpu().numpy())
+           labels = cluster.labels_
+           labels += 1
+           labels[labels > 0] += k
+        #    print("labels: ", min(labels), max(labels))
+           panoptic_labels = torch.zeros(proj_range.shape)
+           panoptic_labels[idx] = torch.Tensor(labels)
+
+        #    print("panoptic labels: ", panoptic_labels, panoptic_labels.min(), panoptic_labels.max())
+           return panoptic_labels
+        #    return cluster
+        # clustering_persons = DBSCAN(eps=3, min_samples=2).fit(X[person_idx])
+
+        cars_cluster = cluster(proj_range, cars_idx, 0)
+        bicycle_cluster = cluster(proj_range, bicycle_idx, 1000)
+        motorcycle_cluster = cluster(proj_range, motorcycle_idx, 2000)
+        truch_cluster = cluster(proj_range, truch_idx, 3000)
+        other_vehicle_cluster = cluster(proj_range, other_vehicle_idx, 4000)
+        person_cluster = cluster(proj_range, person_idx, 5000)
+        bicyclist_cluster = cluster(proj_range, bicyclist_idx, 6000)
+        motorcyclist_cluster = cluster(proj_range, motorcyclist_idx, 7000)
+
+        p_x_cpu = p_x.detach().cpu().numpy()
+        p_y_cpu = p_y.detach().cpu().numpy()
+        
+        unproj_panoptic_cars = cars_cluster[p_y_cpu, p_x_cpu]
+        unproj_panoptic_bicycle = (bicycle_cluster[p_y_cpu, p_x_cpu])
+        unproj_panoptic_motorcycle = motorcycle_cluster[p_y_cpu, p_x_cpu]
+        unproj_panoptic_truch = truch_cluster[p_y_cpu, p_x_cpu]
+        unproj_panoptic_other_vehicle = other_vehicle_cluster[p_y_cpu, p_x_cpu]
+        unproj_panoptic_person = person_cluster[p_y_cpu, p_x_cpu]
+        unproj_panoptic_bicyclist = bicyclist_cluster[p_y_cpu, p_x_cpu]
+        unproj_panoptic_motorcyclist = motorcyclist_cluster[p_y_cpu, p_x_cpu]
+        
+        pred_panoptic = (unproj_panoptic_cars + 
+                         unproj_panoptic_bicycle + 
+                         unproj_panoptic_motorcycle + 
+                         unproj_panoptic_truch + 
+                         unproj_panoptic_other_vehicle +
+                         unproj_panoptic_person +
+                         unproj_panoptic_bicyclist + 
+                         unproj_panoptic_motorcyclist)
+
+        if torch.cuda.is_available():
+            torch.cuda.synchronize()
+        result_time = time.time() - start_time
+        print("DBSCAN", time.time() - start_time, "sec")
+
+        # add panoptic
+        pred_np = (pred_panoptic.detach().cpu().numpy().astype(np.int32) << 16) | pred_np
+
+
+        return pred_np, result_time
+         
